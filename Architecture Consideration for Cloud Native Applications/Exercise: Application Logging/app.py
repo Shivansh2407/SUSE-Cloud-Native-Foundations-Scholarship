db57b64b655cd698e04ba0963a378bd8103eb473
Shivansh Srivastava <shivanshsrivastava2000@gmail.com>
2021-06-19 20:44:06 +0530

    Exercise 2 Solution Added

---
+++ Architecture Consideration for Cloud Native Applications/Exercise: Application Logging/app.py
@@ -0,0 +1,41 @@
+#Shivansh2407
+from flask import Flask
+from flask import json
+import logging
+
+app = Flask(__name__)
+
+@app.route('/status')
+def healthcheck():
+    response = app.response_class(
+            response=json.dumps({"result":"OK - healthy"}),
+            status=200,
+            mimetype='application/json'
+    )
+
+    app.logger.info('Status request successfull')
+    return response
+
+@app.route('/metrics')
+def metrics():
+    response = app.response_class(
+            response=json.dumps({"status":"success","code":0,"data":{"UserCount":140,"UserCountActive":23}}),
+            status=200,
+            mimetype='application/json'
+    )
+
+    app.logger.info('Metrics request successfull')
+    return response
+
+@app.route("/")
+def hello():
+
+    app.logger.info('Main request successfull')
+
+    return "Hello World! Shivansh2407 welcomes you!"
+
+if __name__ == "__main__":
+
+    logging.basicConfig(filename='app.log',level=logging.DEBUG)
+
+    app.run(host='0.0.0.0')
